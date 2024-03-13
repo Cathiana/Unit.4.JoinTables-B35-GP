0b16180f2f389140da47fd2f87417f746729c17d
Cathiana <cathianao@gmail.com>
2024-03-13 16:53:46 -0400

    data buildout tables

--- server/index.js
+++ server/index.js
@@ -1,4 +1,14 @@
-const { person, createTables } = require('./db');
+const { 
+    person,
+     createTables,
+      createUser, 
+      createSkill,
+      createUserSkill,
+      fetchUsers,
+      fetchSkills,
+      fetchUserSkills,
+      destroyUserSkill
+     } = require('./db');
 
 const init = async()=> {
     console.log('connecting to database');
@@ -6,6 +16,32 @@ const init = async()=> {
     console.log('connected to database');
     await createTables();
     console.log('tables created');
+    const [cathy, diane, andrai, dahdoo, writing, publicspeaking, dancing, singing] = await Promise.all([
+        createUser({  username: 'cathy', password:'full'}),
+        createUser({  username: 'diane', password:'stack'}),
+        createUser({  username: 'andrai', password:'academy'}),
+        createUser({  username: 'dahdoo', password:'fsa'}),
+        createSkill({ name: 'writing'}),
+        createSkill({ name: 'publicspeaking'}),
+        createSkill({ name: 'dancing'}),
+        createSkill({ name: 'singing'})
+    ]);
+    console.log(await fetchUsers());
+    console.log(await fetchSkills());
+
+const [cathySinging, cathyPublicspeaking] = await Promise.all ([
+    createUserSkill({ user_id: cathy.id, skill_id: singing.id}),
+    createUserSkill({ user_id: cathy.id, skill_id: publicspeaking.id})
+]);
+console.log(await fetchUserSkills(cathy.id));
+
+//testing for any errors - cathy doesnt sings!
+//await destroyUserSkill({ user_id: diane.id, id: cathySinging.id });
+await destroyUserSkill(cathyPublicspeaking);
+
+//await destroyUserSkill(cathyWriting);
+//console.log(await fetchUserSkills(cathy.id));
+
 };
 
 init();
